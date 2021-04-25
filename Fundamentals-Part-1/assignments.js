47fd7541f5883ed887c474b126479244c17dd16b
Nirav <boghranirav@Niravs-MacBook-Air.local>
2021-04-25 13:16:49 +0530

    Data Types

--- Fundamentals-Part-1/assignments.js
+++ Fundamentals-Part-1/assignments.js
@@ -1,7 +1,21 @@
+//Values and Variables
+
 let country = 'India';
 let continent = 'Asia';
-let population = '130';
+let population = 130;
 
 console.log(country);
 console.log(continent);
 console.log(population);
+
+// Data Types
+
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof country);
+console.log(typeof population);
+console.log(typeof language);
+
+
