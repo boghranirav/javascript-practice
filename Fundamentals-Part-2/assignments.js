992c5b6cb83b253bbf4f1fe6fa687d32444b02cf
Nirav <boghranirav@Niravs-MacBook-Air.local>
2021-04-27 10:55:28 +0530

    Function Declarations vs. Expressions

--- Fundamentals-Part-2/assignments.js
+++ Fundamentals-Part-2/assignments.js
@@ -1,10 +1,27 @@
+'use strict'
+
 // Functions
 
-function describeCountry(country, population, capitalCity) {
-    return (`${country} has ${population} million people and its capital city is ${capitalCity}`);
+// function describeCountry(country, population, capitalCity) {
+//     return (`${country} has ${population} million people and its capital city is ${capitalCity}`);
+// }
+
+// const myCountry = describeCountry('India', '130', 'Delhi');
+// console.log(myCountry);
+// const myPrevCountry = describeCountry('New Zealand', '5.6', 'Wellington');
+// console.log(myPrevCountry);
+
+//Function Declarations vs. Expressions
+
+function percentageOfWorld1(population) {
+    return (population / 7900) * 100;
 }
 
-const myCountry = describeCountry('India', '130', 'Delhi');
-console.log(myCountry);
-const myPrevCountry = describeCountry('New Zealand', '5.6', 'Wellington');
-console.log(myPrevCountry);
+console.log('India : ' + percentageOfWorld1(1300));
+
+const percentageOfWorld2 = function (population) {
+    return (population / 7900) * 100;
+}
+
+console.log(`New Zealand : ${percentageOfWorld2(5.5)}`)
+console.log(`China : ${percentageOfWorld2(1441)}`)
